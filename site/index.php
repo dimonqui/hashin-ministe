6c4efd1d54e25d2cf385cbb58898bb989750f03e
Fede <fede@fedehartman.com>
2012-12-19 14:14:41 -0200

    Maquetación día 1 (?)

--- site/index.php
+++ site/index.php
@@ -1,8 +1,8 @@
 <!doctype html>
 
 <!--
-Site Name: 
-Credits: http://www.com/humans.txt
+Site Name: Montecable 2012
+Credits: http://www.montecable.com/2012/humans.txt
 --> 
 
 
@@ -11,12 +11,12 @@ Credits: http://www.com/humans.txt
 
 <head>
 
-	<title>Bungalows del Piemonte - Complejo Turístico</title>
+	<title>Montecable en el 2012</title>
 
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="description" content="">
-	<meta name="author" content="http://www.com/humans.txt" />
+	<meta name="author" content="http://www.montecable.com/2012/humans.txt" />
 	<meta name="robots" content="all" /> 
 	<meta name="viewport" content="width=device-width">
 
@@ -30,6 +30,8 @@ Credits: http://www.com/humans.txt
 	<link rel="stylesheet" href="css/reset.css">
 	<link rel="stylesheet" href="css/style.css">
 
+	<link type="text/plain" rel="author" href="http://www.montecable.com/2012/humans.txt" />
+
 	<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	<!--[if IE]><link href="css/ie.css" rel="stylesheet"><![endif]-->
 
@@ -39,49 +41,6 @@ Credits: http://www.com/humans.txt
 <body>
 
 <?php include("ie.php"); ?>
-
-
-
-<!--| PELOTITAS (?)
-======================================================================================================================== |-->
-
-<nav class="pelotitas">
-	<div class="pelotita"><a class="anchorLink" href="#home"></a></div>
-	<div class="pelotita pelotita-active"><a class="anchorLink" href="#complejo"></a></div>
-	<div class="pelotita"><a class="anchorLink" href="#aptos"></a></div>
-	<div class="pelotita"><a class="anchorLink" href="#contacto"></a></div>
-	<div class="pelotita"><a class="anchorLink" href="#complejo"></a></div>
-</nav>
-
-
-<!--| MODALES
-======================================================================================================================== |-->
-
-<div class="modal none">
-	<div class="modal-overlay"></div>
-	<div class="modal-content">
-		<div class="modal-header"><h3>Ubicación</h3><canvas></canvas><div class="close"></div></div>
-		<div class="modal-body"></div>
-	</div>
-</div>
-
-
-<!--| HEADER
-======================================================================================================================== |-->
-
-<header>
-	<div class="header_wrapper">
-		<h1><a class="anchorLink" href="#home"><span class="none">Bungalows del Piemonte - Complejo Turístico</span></a></h1>
-		<nav class="main-menu">
-			<ul>
-				<li class="active"><a class="anchorLink" href="#complejo">El Complejo</a></li>
-				<li><a class="anchorLink" href="#aptos">Bungalows & Apartamentos</a></li>
-				<li><a class="anchorLink" href="#contacto">Contacto & Reservas</a></li>
-				<li><a href="promos.php">Promociones & Eventos</a></li>
-			</ul>
-		</nav>
-	</div>
-</header>
 
 
 <!--| MAIN
@@ -106,51 +65,7 @@ Credits: http://www.com/humans.txt
 	<!--| 2 · SERVICIOS
 	==================================================================================================== |-->
 
-	<section class="section servicios">
-		<nav id="complejo"></nav>
 
-		<article class="servicios_top">
-			<figure>
-				<div class="shadow-left"></div>
-				<div class="slider">
-					<img src="img/fotos/servicios/01.jpg" width="450" height="300" alt="Bungalows del Piemonte" class="active">
-					<img src="img/fotos/servicios/02.jpg" width="450" height="300" alt="Bungalows del Piemonte">
-					<img src="img/fotos/servicios/03.jpg" width="450" height="300" alt="Bungalows del Piemonte">
-				</div>
-				<div class="shadow-right"></div>
-			<figure>
-			<div class="servicios-txt">
-				<h3>58 unidades</h3>
-				<p>...en la Playa La Aguada, Rocha, totalmente 
-				equipadas para que nuestros huéspedes sólo tengan que preocuparse por disfrutar de un 
-				entorno donde la belleza natural y la magia del mar garantizan una estadía inolvidable.</p>
-				<div class="botonera">
-					<div class="btn-type-2"><a class="ver-ubicacion" href="#"><i class="ubicacion"></i>Ver Ubicación</a></div>
-					<div class="btn-type-2"><a href="#"><i class="promociones"></i>Promociones y Eventos</a></div>
-				</div>
-			</div>
-		</article>
-
-		<article class="servicios_bot">
-			<div class="servicios_bot-wrapper">
-				<div class="servicios_bot-left">
-					<h3>Servicios</h3>
-					<p>Baño privado en cada unidad&nbsp;&nbsp;–&nbsp;&nbsp;WiFi&nbsp;&nbsp;–&nbsp;&nbsp;Cocheras cubiertas individuales&nbsp;&nbsp;–&nbsp;&nbsp;Kitchenette*&nbsp;&nbsp;–&nbsp;&nbsp;Estufa a leña*&nbsp;&nbsp;–&nbsp;&nbsp;Heladera en cada unidad&nbsp;&nbsp;–&nbsp;&nbsp;Mucama&nbsp;&nbsp;–&nbsp;&nbsp;Parrillero&nbsp;&nbsp;–&nbsp;&nbsp;Piscina&nbsp;&nbsp;–&nbsp;&nbsp;Ropa de cama&nbsp;&nbsp;–&nbsp;&nbsp;TV cable&nbsp;&nbsp;–&nbsp;&nbsp;Vajilla&nbsp;&nbsp;–&nbsp;&nbsp;Caja de seguridad en recepción<br>Servicio despertador&nbsp;&nbsp;–&nbsp;&nbsp;Cobertura médica&nbsp;&nbsp;–&nbsp;&nbsp;Lavandería<br>Teléfono y fax.</p>
-					<small>*Servicio disponible en casi todas las unidades</small>
-				</div>
-				<div class="servicios_bot-right">
-					<figure>
-						<ul>
-							<li><img src="img/fotos/iconos/iconos-01.png" width="350" height="165" alt="Bungalows del Piemonte"></li>
-							<li><img src="img/fotos/iconos/iconos-02.png" width="350" height="165" alt="Bungalows del Piemonte"></li>
-						</ul>
-					</figure>
-					<div class="botonera"><div class="btn-type-2"><a class="anchorLink" href="#aptos">Ver Bungalows & Apartamentos<i></i></a></div></div>
-				</div>
-			</div>
-		</article>
-
-	</section>
 
 
 
@@ -160,152 +75,6 @@ Credits: http://www.com/humans.txt
 	<section class="section sep1"></section>
 
 
-
-	<!--| 3 · APARTAMENTOS
-	==================================================================================================== |-->
-
-	<section class="section aptos">
-		<nav id="aptos"></nav>
-
-
-		<!--| ARRIBA
-		================================================================================= |-->
-
-		<div class="aptos-sep aptos-sep-top">
-			<div class="aptos-left">
-				<h3>Apartamentos</h3>
-				<ul class="aptos-num">
-					<li class="active"><a href="#"><i></i><b>2</b>personas</a></li>
-					<li><a href="#"><i></i><b>3</b>personas</a></li>
-					<li><a href="#"><i></i><b>4</b>personas</a></li>
-					<li><a href="#"><i></i><b>5</b>personas</a></li>
-					<li><a href="#"><i></i><b>6</b>personas</a></li>
-				</ul>
-			</div>
-			<div class="aptos-right">
-
-			
-			<!--| GALERÍA
-			============================================================ |-->
-			
-				<div class="aptos-gallery">
-					<div id="rg-gallery" class="rg-gallery">
-						<div class="rg-thumbs">
-							<div class="es-carousel-wrapper">
-								<div class="es-carousel">
-									<ul>
-										<li><a href="#"><img src="img/fotos/aptos/01.jpg" data-large="img/fotos/aptos/01.jpg" alt="Bungalows del Piemonte" data-description="Bungalows del Piemonte" /></a></li>
-										<li><a href="#"><img src="img/fotos/aptos/02.jpg" data-large="img/fotos/aptos/02.jpg" alt="Bungalows del Piemonte" data-description="Bungalows del Piemonte" /></a></li>
-										<li><a href="#"><img src="img/fotos/aptos/03.jpg" data-large="img/fotos/aptos/03.jpg" alt="Bungalows del Piemonte" data-description="Bungalows del Piemonte" /></a></li>
-										<li><a href="#"><img src="img/fotos/aptos/02.jpg" data-large="img/fotos/aptos/02.jpg" alt="Bungalows del Piemonte" data-description="Bungalows del Piemonte" /></a></li>
-									</ul>
-								</div>
-							</div>
-						</div><!-- rg-thumbs -->
-					</div><!-- rg-gallery -->
-					<div class="apto-botonera">
-						<div class="apto-botonera-line"></div>
-						<div class="btn-type-2"><a class="anchorLink" href="#contacto"><i class="reserva"></i>Reservar</a></div>
-					</div>
-				</div> <!-- aptos-gallery -->
-
-			</div>
-		</div>
-
-
-
-		<!--| ABAJO
-		================================================================================= |-->
-
-		<div class="aptos-sep aptos-sep-bot">
-			<div class="aptos-left">
-				<h3>Bungalows</h3>
-				<ul class="aptos-num">
-					<li><a href="#"><i></i><b>2</b>personas</a></li>
-					<li><a href="#"><i></i><b>3</b>personas</a></li>
-					<li class="active"><a href="#"><i></i><b>4</b>personas</a></li>
-					<li><a href="#"><i></i><b>5</b>personas</a></li>
-					<li><a href="#"><i></i><b>6</b>personas</a></li>
-				</ul>
-			</div>
-
-
-			<!--| GALERÍA
-			============================================================ |-->
-
-			<div class="aptos-right">
-				<div class="aptos-gallery">
-					<div id="rg-gallery-2" class="rg-gallery rg-gallery">
-						<div class="rg-thumbs">
-							<div class="es-carousel-wrapper">
-								<div class="es-carousel">
-									<ul>
-										<li><a href="#"><img src="img/fotos/aptos/01.jpg" data-large="img/fotos/aptos/01.jpg" alt="Bungalows del Piemonte" data-description="Bungalows del Piemonte" /></a></li>
-										<li><a href="#"><img src="img/fotos/aptos/02.jpg" data-large="img/fotos/aptos/02.jpg" alt="Bungalows del Piemonte" data-description="Bungalows del Piemonte" /></a></li>
-										<li><a href="#"><img src="img/fotos/aptos/03.jpg" data-large="img/fotos/aptos/03.jpg" alt="Bungalows del Piemonte" data-description="Bungalows del Piemonte" /></a></li>
-										<li><a href="#"><img src="img/fotos/aptos/02.jpg" data-large="img/fotos/aptos/02.jpg" alt="Bungalows del Piemonte" data-description="Bungalows del Piemonte" /></a></li>
-									</ul>
-								</div>
-							</div>
-						</div><!-- rg-thumbs -->
-					</div><!-- rg-gallery -->
-					<div class="apto-botonera">
-						<div class="apto-botonera-line"></div>
-						<div class="btn-type-2"><a class="anchorLink" href="#contacto"><i class="reserva"></i>Reservar</a></div>
-					</div>
-				</div> <!-- aptos-gallery -->
-
-			</div>
-		</div>
-	</section>
-
-
-
-	<!--| B· SEPARADOR 2
-	==================================================================================================== |-->
-
-	<section class="section sep2"></section>
-
-
-
-	<!--| 4 · CONTACTO
-	==================================================================================================== |-->
-
-	<section class="section contacto">
-		<nav id="contacto"></nav>
-
-		<div class="contacto-wrapper">
-			<h3>Contacto & Reservas<i></i></h3>
-			<form>
-				<div class="contacto-content">
-					<div class="contacto-content-top">
-						<div class="contacto-content-left">
-							<label for="nombre">Nombre<input type="text" name="nombre" class="nombre"></label>
-							<label for="email">Email<input type="email" name="email" class="email"></label>
-							<label for="tel">Teléfono<input type="tel" name="tel" class="tel"></label>
-							<p>Seleccione el motivo del contacto<i></i></p>
-							<div class="contacto-botonera">
-								<div class="btn-type-2 left"><a href="#"><i class="reserva"></i>Reserva</a></div>
-								<div class="btn-type-2 left"><a href="#"><i class="consulta"></i>Consulta</a></div>
-							</div>
-						</div>
-						<div class="contacto-content-right">
-							<label>Mensaje:</label>
-							<textarea></textarea>
-						</div>
-					</div>
-					<div class="lines"><canvas class="lines-top"></canvas><canvas class="lines-bot"></canvas></div>
-					<div class="contacto-content-bot">
-						<label class="personas" for="personas">Nº de personas<input type="number" name="personas" class="personas"></label>
-						<label class="entrada" for="entrada">Fecha de Entrada<input type="date" name="entrada" class="entrada"></label>
-						<label class="salida" for="salida">Fecha de Salida<input type="date" name="salida" class="salida"></label>
-						<input class="enviar" type="submit" value="Enviar">
-					</div>
-				</div>
-			</form>
-		</div>
-
-	</section>
 
 
 
@@ -323,11 +92,7 @@ Credits: http://www.com/humans.txt
 
 <footer>
 	<div class="footer_wrapper">
-		<p>Balneario La Aguada. La Paloma, Uruguay&nbsp;&nbsp;|&nbsp;&nbsp;Tel: +598·4479·6096, +598·4479·6520&nbsp;&nbsp;|&nbsp;&nbsp;Fax: +598·4479·6922</p>
-		<div class="footer-right">
-			<nav class="volverinicio"><a class="anchorLink" href="#home">Volver al inicio<i></i></a></nav>
-			<div class="iugo"><a href="http://www.iugo.com.uy"><span class="none">Iugo</span></a></div>
-		</div>
+		
 	</div>
 </footer>
 
