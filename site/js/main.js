b29769513e1e0695a020093837c740c9ace6db5a
nicolas <nicolasmedeiros@gmail.com>
2012-12-21 10:54:49 -0200

    con lo ultimo de fede y correccion para q en reload vaya al inicio

--- site/js/main.js
+++ site/js/main.js
@@ -3,19 +3,23 @@ $(document).ready(function() {
 /*--| QUERY LOADER
 ----------------------------------------------------------------------------------------------------------------------- |--*/
 
-/*$("body").queryLoader2({
-	barColor: "#dbc16a",
-	backgroundColor: "#2d2d2d",
+$("body").queryLoader2({
+	barColor: "#e10c0e",
+	backgroundColor: "#1c1c1c",
 	percentage: true,
 	barHeight: 30,
 	completeAnimation: "grow"
-});*/
+	});
 
 
 
 /*--| ANCHORS
 ------------------------------------------------------------------------------------------------------------------------ |--*/
 
-$("a.anchorLink").anchorAnimate()
+$("a.anchorLink").anchorAnimate();
+
+//cada vez que se recarga la página, comenzar desde cero:
+	$("html, body").animate({ scrollTop: 0 },{duration: 500, queue: false} );
+
 
 });
